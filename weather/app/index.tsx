5422477387095e98f33fab8bcd60636ce6108623
PALLAB GHOSH <gpallab405@gmail.com>
2024-07-22 09:51:11 +0530

    fetch the wather data using api and url

--- weather/app/index.tsx
+++ weather/app/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const index = () => {
   return (
     <View>
       <Text>index</Text>
+      <Link href={'/weather'}>go to weather</Link>
     </View>
   )
 }
--- weather/app/weather.tsx
+++ weather/app/weather.tsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-const weather = () => {
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=22.8010&lon=88.3699&appid=202df2cc776dd5986ec378b81f3013bd&units=metric`
+
+
+
+const Weather = () => {
+  
+    const [weather , setweather] = useState({});
+
+
+  const fetchweather = async()=>{
+     const result = await  fetch(url);
+     const data = await result.json()
+     setweather(data)
+     console.log(data)
+  }
+
+  useEffect(()=>{
+    fetchweather();
+  },[])
+
+
+if(!weather)
+{
+    return <ActivityIndicator/>
+}
+
   return (
     <View>
-      <Text>weather</Text>
+      <Text>{weather.name}</Text>
     </View>
   )
 }
 
-export default weather
+export default Weather
 
 const styles = StyleSheet.create({})
